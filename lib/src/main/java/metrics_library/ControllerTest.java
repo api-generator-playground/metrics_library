c52c1a6d66bf87400f27a0693f389efce4450752
davibss <davi.sousa@ccc.ufcg.edu.br>
2022-06-28 11:52:13 -0300

    adding generic metrics endpoint and activating all endpoints of actuator

--- lib/src/main/java/metrics_library/ControllerTest.java
+++ lib/src/main/java/metrics_library/ControllerTest.java
@@ -3,6 +3,8 @@ package metrics_library;
 import io.prometheus.client.Collector;
 import io.prometheus.client.CollectorRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.MetricsEndpoint;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,6 +22,9 @@ public class ControllerTest {
     @Autowired
     private CollectorRegistry collectorRegistry;
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @GetMapping
     public ResponseEntity<?> getMetrics() {
         return new ResponseEntity<>("test metrics", HttpStatus.OK);
@@ -32,6 +37,18 @@ public class ControllerTest {
         this.collectorRegistry.metricFamilySamples().asIterator().forEachRemaining(samplesList::add);
 
         return new ResponseEntity<>(samplesList, HttpStatus.OK);
+    }
+
+    @GetMapping("/generic")
+    public ResponseEntity<?> getGenericMetrics() {
+
+        MetricsEndpoint metricsEndpoint = (MetricsEndpoint) applicationContext.getBean("metricsEndpoint", MetricsEndpoint.class);
+
+        List<MetricsEndpoint.MetricResponse> metrics = new ArrayList<>();
+
+        metricsEndpoint.listNames().getNames().iterator().forEachRemaining(name -> metrics.add(metricsEndpoint.metric(name, null)));
+
+        return new ResponseEntity<>(metrics, HttpStatus.OK);
     }
 
 }
